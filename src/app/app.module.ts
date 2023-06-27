fe8b533db0a3e38275ce507d07b2355e81775eb3
Frendlyindy <frendlyindian@gmail.com>
2023-06-26 19:25:55 -0600

    routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { ToolsComponent } from './tools/tools.component';
+import { InfoComponent } from './info/info.component';
+import { LifestyleComponent } from './lifestyle/lifestyle.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    ToolsComponent,
+    InfoComponent,
+    LifestyleComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      { path: 'home', component: HomeComponent },
+      { path: 'tools', component: ToolsComponent },
+      { path: 'info', component: InfoComponent },
+      { path: 'lifestyle', component: LifestyleComponent },
+      { path: '', redirectTo: '/home', pathMatch: 'full' },
+    ]),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
